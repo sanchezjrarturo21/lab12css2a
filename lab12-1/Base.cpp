9011b848fe29daa07b1b1f12d9552973cd45e524
sanchezjrarturo21 <46791369+sanchezjrarturo21@users.noreply.github.com>
2019-04-25 09:25:07 -0700

    initial commit

---
+++ lab12-1/Base.cpp
@@ -0,0 +1,10 @@
+// Part1.cpp. Note this implementation file will have TWO class defined.
+#include "Part1.h"
+void Base::testFunction ()
+{
+cout << "Base class" << endl;
+}
+void Derived::testFunction ()
+{
+cout << "Derived class" << endl;
+}
